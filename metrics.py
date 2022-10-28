ba803978981afdfe715c023af21ef2bae6121f13
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2022-10-28 20:04:38 +0200

    Added squared_deviation and absolute_deviation to the metrics

--- metrics.py
+++ metrics.py
@@ -3,30 +3,34 @@ import numpy as np
 
 def accuracy(y_pred, y_true):
     n_objects = y_pred.shape[0]
-    cnt = [y_pred[i] == y_true[i] for i in range(n_objects)]
+    cnt = len([y_pred[i] == y_true[i] for i in range(n_objects)])
     return cnt / n_objects
+
 
 def precision(y_pred, y_true):
     n_objects = y_pred.shape[0]
-    
+
     tp = len([y_pred[i] == True and y_true[i] == True for i in range(n_objects)])
     fp = len([y_pred[i] == True and y_true[i] == False for i in range(n_objects)])
 
     return tp / (tp + fp)
 
+
 def recall(y_pred, y_true):
     n_objects = y_pred.shape[0]
-    
+
     tp = len([y_pred[i] == True and y_true[i] == True for i in range(n_objects)])
     fn = len([y_pred[i] == False and y_true[i] == True for i in range(n_objects)])
 
     return tp / (tp + fn)
 
-def f1_score(y_pred, y_true):
-    precision = precision(y_pred, y_true)
-    recall = recall(y_pred, y_true)
 
-    return 2 * precision * recall / (recall + precision)
+def f1_score(y_pred, y_true):
+    pre = precision(y_pred, y_true)
+    rec = recall(y_pred, y_true)
+
+    return 2 * pre * rec / (rec + pre)
+
 
 def entropy(y):
     """
@@ -45,9 +49,10 @@ def entropy(y):
 
     EPS = 0.0005
 
-    p = np.mean(y, axis = 0)
+    p = np.mean(y, axis=0)
 
     return -np.sum(p * np.log2(p + EPS))
+
 
 def gini(y):
     """
@@ -64,6 +69,15 @@ def gini(y):
         Gini impurity of the provided subset
     """
 
-    p = np.mean(y, axis = 0)
+    p = np.mean(y, axis=0)
     return -np.sum(p * p) + 1
 
+
+def squared_deviation(y):
+    y_mean = np.mean(y, axis=0)
+    return np.mean((y - y_mean) ** 2)
+
+
+def absolute_deviation(y):
+    y_median = np.median(y, axis=0)
+    return np.mean(np.abs(y - y_median))
