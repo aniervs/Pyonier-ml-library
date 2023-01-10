d7291300bd4312136cc0fc608d0eaddaad32d5e4
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-01-10 06:58:35 +0100

    Add Basic Autograd to the Tensor.
    TO DO:
    - Test backward method.
    - Support having a vertex with several outputs. This means only back-propagate to the parents of U after all children of U have propagated to U.

--- nn/Tensor.py
+++ nn/Tensor.py
@@ -2,8 +2,26 @@ import numpy as np
 
 
 class Tensor:
-    def __init__(self, data):
+    def __init__(self, data, parents, parents_operation):
         self.data = np.array(data)
+        self.parents = parents
+        self.parents_operation = parents_operation
+        self.grad = None
+
+    def backward(self, grad):
+        self.grad = grad
+
+        if self.parents_operation == "add":
+            self.parents[0].backward(grad)
+            self.parents[1].backward(grad)
+        elif self.parents_operation == "sub":
+            self.parents[0].backward(grad)
+            self.parents[1].backward(-grad)
+        elif self.parents_operation == "neg":
+            self.parents[0].backward(-grad)
+        elif self.parents_operation == "mul":
+            self.parents[0].backward(self.grad * self.parents[1])
+            self.parents[1].backward(self.grad * self.parents[0])
 
     def __repr__(self):
         return self.data.__repr__()
@@ -12,13 +30,13 @@ class Tensor:
         return self.data.__str__()
 
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        return Tensor(self.data + other.data, parents=[self, other], parents_operation="add")
 
     def __neg__(self):
-        return Tensor(self.data * -1)
+        return Tensor(self.data * -1, parents=[self], parents_operation="neg")
 
     def __sub__(self, other):
-        return Tensor(self.data - other.data)
+        return Tensor(self.data - other.data, parents=[self, other], parents_operation="sub")
 
     def __mul__(self, other):
-        return Tensor(self.data * other.data)
+        return Tensor(self.data * other.data, parents=[self, other], parents_operation="mul")
--- tests/testTensor.py
+++ tests/testTensor.py
@@ -7,25 +7,33 @@ from nn.Tensor import Tensor
 
 class TestTensor(TestCase):
     def setUp(self) -> None:
-        self.tensor1 = Tensor([1, 2, 3, 4])
-        self.tensor2 = Tensor([5, 6, 7, 8])
+        self.tensor1 = Tensor([1, 2, 3, 4], None, None)
+        self.tensor2 = Tensor([5, 6, 7, 8], None, None)
 
     def testAddition(self):
         new_tensor = self.tensor1 + self.tensor2
-        expected = Tensor([6, 8, 10, 12])
+        expected = Tensor([6, 8, 10, 12], [self.tensor1, self.tensor2], "add")
         self.assertEqual(np.all(expected.data - new_tensor.data), 0)
+        self.assertEqual(expected.parents, new_tensor.parents)
+        self.assertEqual(expected.parents_operation, new_tensor.parents_operation)
 
     def testSubtraction(self):
         new_tensor = self.tensor1 - self.tensor2
-        expected = Tensor([-4, -4, -4, -4])
+        expected = Tensor([-4, -4, -4, -4], [self.tensor1, self.tensor2], "sub")
         self.assertEqual(np.all(expected.data - new_tensor.data), 0)
+        self.assertEqual(expected.parents, new_tensor.parents)
+        self.assertEqual(expected.parents_operation, new_tensor.parents_operation)
 
     def testMultiplication(self):
         new_tensor = self.tensor1 * self.tensor2
-        expected = Tensor([5, 12, 21, 32])
+        expected = Tensor([5, 12, 21, 32], [self.tensor1, self.tensor2], "mul")
         self.assertEqual(np.all(expected.data - new_tensor.data), 0)
+        self.assertEqual(expected.parents, new_tensor.parents)
+        self.assertEqual(expected.parents_operation, new_tensor.parents_operation)
 
     def testNegation(self):
         new_tensor = -self.tensor1
-        expected = Tensor([-1, -2, -3, -4])
+        expected = Tensor([-1, -2, -3, -4], [self.tensor1], "neg")
         self.assertEqual(np.all(expected.data - new_tensor.data), 0)
+        self.assertEqual(expected.parents, new_tensor.parents)
+        self.assertEqual(expected.parents_operation, new_tensor.parents_operation)
