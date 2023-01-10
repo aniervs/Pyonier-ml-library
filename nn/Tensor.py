da92ce03fc3dfdaa76ada1b063e8252a238f4bf3
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-01-10 06:05:06 +0100

    Create the Tensor initial class, with no autograd nor forward/backward passing.

---
+++ nn/Tensor.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class Tensor:
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return self.data.__repr__()
+
+    def __str__(self):
+        return self.data.__str__()
+
+    def __add__(self, other):
+        return Tensor(self.data + other.data)
+
+    def __neg__(self):
+        return Tensor(self.data * -1)
+
+    def __sub__(self, other):
+        return Tensor(self.data * other.data)
+
+    def __mul__(self, other):
+        return Tensor(self.data * other.data)
