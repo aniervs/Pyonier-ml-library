ae0234ea50aab040619a61bd60940a9ba971fb5d
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-01-16 06:05:28 +0100

    Make the backpropagation to not recurse through a vertex several times. It only goes to its parents after all its children have propagated to it.

--- nn/Tensor.py
+++ nn/Tensor.py
@@ -7,26 +7,48 @@ class Tensor:
         self.parents = parents
         self.parents_operation = parents_operation
         self.grad = None
+        self.children = None
+        if self.parents is not None:
+            for parent in self.parents:
+                if parent.children is None:
+                    parent.children = dict()
+                if self not in parent.children:
+                    parent.children[self] = 0
+                parent.children[self] += 1
 
-    def backward(self, grad=None):
+    def backward(self, grad=None, grad_vertex=None):
         if grad is None:
             grad = Tensor([1])
+
         if self.grad is None:
             self.grad = Tensor([0])
 
         self.grad = self.grad + grad
+        if self.children is not None:
+            self.children[grad_vertex] -= 1
+
+        if self.children is not None and max(self.children.values()) != 0:
+            return
+
+        if self.parents is None or self.parents_operation is None:
+            return
 
         if self.parents_operation == "add":
-            self.parents[0].backward(grad)
-            self.parents[1].backward(grad)
+            self.parents[0].backward(grad, self)
+            self.parents[1].backward(grad, self)
         elif self.parents_operation == "sub":
-            self.parents[0].backward(grad)
-            self.parents[1].backward(-grad)
+            self.parents[0].backward(grad, self)
+            self.parents[1].backward(-grad, self)
         elif self.parents_operation == "neg":
-            self.parents[0].backward(-grad)
+            self.parents[0].backward(-grad, self)
         elif self.parents_operation == "mul":
-            self.parents[0].backward(self.grad * self.parents[1])
-            self.parents[1].backward(self.grad * self.parents[0])
+            self.parents[0].backward(self.grad * self.parents[1], self)
+            self.parents[1].backward(self.grad * self.parents[0], self)
+
+        for parent in self.parents:
+            if self not in parent.children:
+                parent.children[self] = 0
+            parent.children[self] += 1
 
     def __repr__(self):
         return self.data.__repr__()
