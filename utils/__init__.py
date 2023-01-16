df88177c7589a65456e2d306728b9e3064f842a6
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-01-17 00:28:24 +0100

    Minor structure change.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def count_number_calls(f):
     def wrapped(*args, **kwargs):
         wrapped.calls += 1
@@ -5,3 +8,13 @@ def count_number_calls(f):
 
     wrapped.calls = 0
     return wrapped
+
+
+def one_hot_encode(n_classes, y):
+    y_one_hot = np.zeros((len(y), n_classes), dtype=float)  # creates a (n_objects, n_classes) matrix
+    y_one_hot[np.arange(len(y)), y.astype(int)[:, 0]] = 1.  # sets to 1 the corresponding class for each object
+    return y_one_hot
+
+
+def one_hot_decode(y_one_hot):
+    return y_one_hot.argmax(axis=1)[:, None]
