Minor fix to the backpropagation.

nn/Tensor.py
@@ -6,33 +6,31 @@ unary_operations = {"neg", "pow", "transpose", "expand", "sum", "relu"}
 
 
 class Tensor:
-    def __init__(self, data, autograd=False, parents=None, parents_operation=None):
+    def __init__(self, data, autograd=False, parents=None, parents_operation=None, id=None):
         self.data = np.array(data)
         self.autograd = autograd
         self.parents = parents
         self.parents_operation = parents_operation
         self.grad = None
         self.children = dict()
-        self.__update_parents()
+
+        if self.parents is None:
+            return
+        if id is None:
+            self.id = np.random.randint(0, 100000)
+        else:
+            self.id = id
+
+        for parent in self.parents:
+            if self.id not in parent.children:
+                parent.children[self.id] = 0
+            parent.children[self.id] += 1
 
     def __repr__(self):
         return self.data.__repr__()
 
     def __str__(self):
         return self.data.__str__()
-
-    def __update_parents(self):
-        if self.parents is None:
-            return
-
-        if self not in self.parents[0].children:
-            self.parents[0].children[self] = 0
-        self.parents[0].children[self] += 1
-
-        if self.parents_operation not in unary_operations:
-            if self not in self.parents[1].children:
-                self.parents[1].children[self] = 0
-            self.parents[1].children[self] += 1
 
     def backward(self, grad=None, grad_vertex=None):
         if not self.autograd:
@@ -46,16 +44,16 @@ class Tensor:
         self.grad = self.grad + grad
 
         if grad_vertex is not None:
-            if self.children[grad_vertex] == 0:
+            if self.children[grad_vertex.id] == 0:
                 raise Exception('Cannot Back propagate any more')
-            self.children[grad_vertex] -= 1
+            self.children[grad_vertex.id] -= 1
 
         assert not grad.autograd
 
         if self.parents is None or self.parents_operation is None:
             return
 
-        if len(self.children) != 0 and max(self.children.values()) != 0:
+        if grad_vertex is not None and len(self.children) != 0 and max(self.children.values()) > 0:
             return
 
         if self.parents_operation == "add":
@@ -69,27 +67,25 @@ class Tensor:
         elif self.parents_operation == "mul":
             self.parents[0].backward(self.grad * self.parents[1], self)
             self.parents[1].backward(self.grad * self.parents[0], self)
-        elif self.parents_operation == "pow":
-            power = self.parents[1]
+        elif self.parents_operation[:3] == "pow":
+            power = float(self.parents_operation[4:])
             new_tensor = self.grad * (self.parents[0] ** (power - 1))
             new_tensor.data *= power
             self.parents[0].backward(new_tensor, self)
         elif self.parents_operation == "transpose":
             self.parents[0].backward(self.grad.transpose(), self)
         elif self.parents_operation[:3] == "sum":
-            dim = self.parents[1]
+            dim = int(self.parents_operation[4:])
             ds = self.parents[0].data.shape[dim]
             self.parents[0].backward(self.grad.expand(dim, ds), self)
         elif self.parents_operation[:6] == "expand":
-            dim = self.parents[1]
+            dim = int(self.parents_operation[7:])
             self.parents[0].backward(self.grad.sum(dim), self)
         elif self.parents_operation == "matmul":
             self.parents[0].backward(self.grad @ self.parents[1].transpose(), self)
             self.parents[1].backward((self.grad.transpose() @ self.parents[0]).transpose(), self)
         elif self.parents_operation == "relu":
             self.parents[0].backward(self.grad * Tensor(self.parents[0].data > 0), self)
-
-        self.__update_parents()
 
     def __add__(self, other):
         if self.autograd and other.autograd:
@@ -113,7 +109,7 @@ class Tensor:
 
     def __pow__(self, power):
         if self.autograd:
-            return Tensor(self.data ** power, autograd=True, parents=[self, power], parents_operation="pow")
+            return Tensor(self.data ** power, autograd=True, parents=[self], parents_operation=f"pow_{power}")
         return Tensor(self.data ** power)
 
     def transpose(self):
@@ -123,7 +119,7 @@ class Tensor:
 
     def sum(self, dim):
         if self.autograd:
-            return Tensor(self.data.sum(dim), autograd=True, parents=[self, dim], parents_operation="sum")
+            return Tensor(self.data.sum(dim), autograd=True, parents=[self], parents_operation=f"sum_{dim}")
         return Tensor(self.data.sum(dim))
 
     def expand(self, dim, copies):
@@ -134,7 +130,7 @@ class Tensor:
         new_data = new_data.transpose(trans_cmd)
 
         if self.autograd:
-            return Tensor(new_data, autograd=True, parents=[self, dim], parents_operation="expand")
+            return Tensor(new_data, autograd=True, parents=[self], parents_operation=f"expand_{dim}")
         return Tensor(new_data)
 
     def __matmul__(self, other):

tests/testTensor.py
@@ -45,7 +45,7 @@ class TestTensor(unittest.TestCase):
 
     def testPower(self):
         new_tensor = self.tensor1 ** 3
-        expected = Tensor([1, 8, 27, 64], parents=[self.tensor1, 3], parents_operation="pow")
+        expected = Tensor([1, 8, 27, 64], parents=[self.tensor1], parents_operation="pow_3")
         self.assertEqual(np.all(expected.data - new_tensor.data), 0)
         self.assertEqual(expected.parents, new_tensor.parents)
         self.assertEqual(expected.parents_operation, new_tensor.parents_operation)
@@ -53,7 +53,7 @@ class TestTensor(unittest.TestCase):
 
     def testExpand(self):
         new_tensor = self.tensor1.expand(dim=1, copies=2)
-        expected = Tensor([[1, 1], [2, 2], [3, 3], [4, 4]], parents=[self.tensor1, 1], parents_operation="expand")
+        expected = Tensor([[1, 1], [2, 2], [3, 3], [4, 4]], parents=[self.tensor1], parents_operation="expand_1")
         self.assertEqual(expected.data.shape, new_tensor.data.shape)
         self.assertEqual(np.all(expected.data - new_tensor.data), 0)
         self.assertEqual(expected.parents, new_tensor.parents)
@@ -63,7 +63,7 @@ class TestTensor(unittest.TestCase):
     def testSum(self):
         tensor = Tensor([[1, 5], [2, 6], [3, 7], [4, 8]], autograd=True)
         new_tensor = tensor.sum(dim=1)
-        expected = Tensor([6, 8, 10, 12], autograd=True, parents=[tensor, 1], parents_operation="sum")
+        expected = Tensor([6, 8, 10, 12], autograd=True, parents=[tensor], parents_operation="sum_1")
         self.assertEqual(expected.data.shape, new_tensor.data.shape)
         self.assertEqual(np.all(expected.data - new_tensor.data), 0)
         self.assertEqual(expected.parents, new_tensor.parents)